bbe4dead10f6b601b0170aaf9a3360ac0efd53a4
epicmatthew21 <36817140+epicmatthew21@users.noreply.github.com>
2019-10-25 15:02:08 +0100

    Update Program.cs

--- RobloxVisitBot/Program.cs
+++ RobloxVisitBot/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,17 +11,54 @@ namespace RobloxVisitBot
 {
     class Program
     {
+        static string CSRFToken(string cookie)
+        {
+            try
+            {
+                WebClient wb = new WebClient();
+                wb.Headers["Cookie"] = ".ROBLOSECURITY=" + cookie;
+                Regex regex = new Regex("Roblox.XsrfToken.*?\'(.*)\'");
+                Match matched = regex.Match(wb.DownloadString("https://www.roblox.com/groups/4539492/The-Republic-Navy#!/about"));
+
+                if (matched.Success)
+                {
+                    return matched.Groups[1].Value;
+                }
+                else
+                {
+                    return "Failed";
+                }
+
+
+            }
+            catch (WebException ex)
+            {
+                return "Failed";
+
+            }
+
+
+        }
+        static string Grab_Auth_Ticket(long id)
+        {
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://auth.roblox.com/v1/authentication-ticket/");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            httpWebRequest.Headers.Add(HttpRequestHeader.Cookie, string.Format(".ROBLOSECURITY={0}", System.IO.File.ReadAllText("Cookie.txt")));
+            httpWebRequest.Headers.Add("X-CSRF-TOKEN", CSRFToken(System.IO.File.ReadAllText("Cookie.txt")));
+            httpWebRequest.Referer = "https://www.roblox.com/games/" +id;
+            httpWebRequest.ContentLength = 0;
+            using (WebResponse response = httpWebRequest.GetResponse())
+            {
+                string limit = response.Headers["RBX-Authentication-Ticket"].ToString();
+                return limit;
+            }
+        }
         static void VisitBot(long hu)
         {
-            System.Net.WebClient wb = new System.Net.WebClient();
-            string text = System.IO.File.ReadAllText("Cookie.txt");
-
-            string Cookie = ".ROBLOSECURITY=" + text;
-            wb.Headers["Cookie"] = Cookie;
-            wb.Headers["User-Agent"] = "Roblox/WinInet";
-            wb.Headers["Referer"] = "https://www.roblox.com/develop";
-            wb.Headers["RBX-For-Gameauth"] = "true";
-            string auth_ticket = wb.DownloadString("https://www.roblox.com/game-auth/getauthticket");
+            string auth_ticket = Grab_Auth_Ticket(hu);
             Random rn = new Random();
             int Randomm = rn.Next(1000000, 10000000);
             string url = "roblox-player:1+launchmode:play+gameinfo:" + auth_ticket + "+launchtime:" + Randomm + "+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26browserTrackerId%3D" + Randomm + "%26placeId%3D" + hu + "%26isPlayTogetherGame%3Dfalse+browsertrackerid:" + Randomm + "+robloxLocale:en_us+gameLocale:en_us";
@@ -32,7 +71,7 @@ namespace RobloxVisitBot
             Console.WriteLine("Whats Your Game ID?");
             string Input = Console.ReadLine();
             long a;
-            a = Convert.ToInt64(Input.ToString());
+            a = long.Parse(Input);
             Console.Title = "Visits Given " + Value;
             bool oof = true;
             while (oof == true)
